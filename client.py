e971be658a1630dcf6ae1d81d0142c38fc590cf3
bo233 <ludingbo@hotmail.com>
2018-08-11 23:30:14 +0800

    2018.8.11

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import socket
 
 obj = socket.socket()
 
-obj.connect(("192.168.1.9",8080))
+obj.connect(("192.168.1.3",8080))
 #obj.connect(("172.0.0.1",8080))
 
 ret_bytes = obj.recv(1024)
@@ -10,12 +10,15 @@ ret_str = str(ret_bytes,encoding="utf-8")
 print(ret_str)
 
 while True:
-    inp = input("Hello.Any questions? \n>>>")
-    if inp == "q":
-        obj.sendall(bytes(inp,encoding="utf-8"))
-        break
-    else:
-        obj.sendall(bytes(inp, encoding="utf-8"))
-        ret_bytes = obj.recv(1024)
-        ret_str = str(ret_bytes,encoding="utf-8")
-        print(ret_str)
+	inp = input(">>>")
+	if inp == "quit":
+		obj.sendall(bytes(inp,encoding="utf-8"))
+		break
+	elif inp =="stop":
+		obj.sendall(bytes(inp,encoding="utf-8"))
+		break
+	else:
+		obj.sendall(bytes(inp, encoding="utf-8"))
+		ret_bytes = obj.recv(1024)
+		ret_str = str(ret_bytes,encoding="utf-8")
+		print(ret_str)
--- server.py
+++ server.py
@@ -1,18 +1,67 @@
 import  socketserver
+import socket
 
+def get_host_ip():
+	"""
+	查询本机ip地址
+	:return: ip
+	"""
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		s.connect(('8.8.8.8', 80))
+		ip = s.getsockname()[0]
+	finally:
+		s.close()
+
+	return ip
+
+'''
+def get_host_ip():
+	#获取本机电脑名
+	myname = socket.getfqdn(socket.gethostname(  ))
+	#获取本机ip
+	myaddr = socket.gethostbyname(myname)
+	return myaddr
+'''
+	
+host_ip = get_host_ip()
+	
 class Myserver(socketserver.BaseRequestHandler):
 
-    def handle(self):
+	def handle(self):
+		global host_ip
+		conn = self.request
+		conn.sendall(bytes("From"+host_ip+":Connected successfully.",encoding="utf-8"))
+		print(self.client_address[0]+":"+str(self.client_address[1])+" is connected.")
+		while True:
+			ret_bytes = conn.recv(1024)
+			ret_str = str(ret_bytes,encoding="utf-8")
+			if ret_str == "quit":
+				print(self.client_address[0]+":"+str(self.client_address[1])+" is disconnected.")
+				conn.sendall(bytes(" ",encoding="utf-8"))
+				break
+			elif ret_str == "stop":
+				self.server.shutdown()
+				self.request.close()
+				conn.sendall(bytes(" ",encoding="utf-8"))
+				break
+			elif ret_str == "help":
+				conn.sendall(bytes("quit:disconnecte with host\nstop:turn off the server",encoding="utf-8"))
+			else:
+				conn.sendall(bytes("Please input correct commends, type 'help' for details.",encoding="utf-8"))
 
-        conn = self.request
-        conn.sendall(bytes("Hello, I'm a robot.",encoding="utf-8"))
-        while True:
-            ret_bytes = conn.recv(1024)
-            ret_str = str(ret_bytes,encoding="utf-8")
-            if ret_str == "q":
-                break
-            conn.sendall(bytes(ret_str+"Hello, everyone.",encoding="utf-8"))
 
+#为了复用地址，自定义一个类以设置allow_reuse_address = True
+class EchoServer(socketserver.ThreadingTCPServer):
+	allow_reuse_address = True
+	daemon_threads = True
+	def __init__(self, server_address, RequestHandlerClass):
+		socketserver.ThreadingTCPServer.__init__(self, server_address, RequestHandlerClass)
+			
 if __name__ == "__main__":
-    server = socketserver.ThreadingTCPServer(("127.0.0.1",8080),Myserver)
-    server.serve_forever()
+	try:
+		server = EchoServer((host_ip,8080),Myserver)
+		#server = socketserver.ThreadingTCPServer(('192.168.1.9',8080),Myserver)
+		server.serve_forever()
+	except Exception as e:
+		print('except:', e)
